6b6f08253068619928b6a843adf127d459a21795
rehanzhakyy <hannn9491@gmail.com>
2024-05-12 23:53:20 +0700

    update Modules Numbers

--- 01-Kotlin-Basics/06-Numbers.kt
+++ 01-Kotlin-Basics/06-Numbers.kt
@@ -4,7 +4,15 @@ fun main() {
     val a: Byte = -128
     val b: Byte = 127
 
+    // Short
+    val c: Short = -32768
+    val d: Short = 32767
+
     // Mencetak Bytes
-    print(a)
-    print(b)
+    println(a)
+    println(b)
+
+    // Mencetak Short
+    println(c)
+    println(d)
 }
