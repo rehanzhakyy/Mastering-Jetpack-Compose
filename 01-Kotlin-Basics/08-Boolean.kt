790fb8fcc278a57a2a40389d8db3c863bb362503
rehanzhakyy <hannn9491@gmail.com>
2024-05-13 00:45:46 +0700

    update Modules Boolean

--- 01-Kotlin-Basics/08-Boolean.kt
+++ 01-Kotlin-Basics/08-Boolean.kt
@@ -1,5 +1,12 @@
 fun main() {
     // Boolean
-    val isKotlinEz = true
+    val isKotlinEz: Boolean = true
     println(isKotlinEz)
+
+    // Boolean Expressions
+    val a = 1
+    val b = 2
+
+    println(a > b)
+
 }
