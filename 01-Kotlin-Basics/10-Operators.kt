1bf86f9aa7150b8afaa96739716c7db820018c43
rehanzhakyy <hannn9491@gmail.com>
2024-05-21 22:24:22 +0700

    update kotlin operators

--- 01-Kotlin-Basics/10-Operators.kt
+++ 01-Kotlin-Basics/10-Operators.kt
@@ -1,10 +1,26 @@
 fun main() {
-    var a = 3
-    var b = 5
-
+    // Arithmetic Operators
+    val a = 8
+    val b = 2
+    // Arithmetic Operators Output
     println(a + b)
     println(a - b)
     println(a * b)
     println(a / b)
     println(a % b)
+
+    // Assignment Operators
+    var c = 10
+    c += 6
+    c -= 6
+    c /= 6
+    c %= 6
+
+    // Assignment Operators Output
+    println(c)
+
+    val k: Float = 7f
+    val l: Float = 3f
+
+    println(k / l)
 }
