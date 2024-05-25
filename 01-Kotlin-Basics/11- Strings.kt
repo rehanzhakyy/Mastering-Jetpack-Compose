5662c5cc2698c716bd79906a8553b50eb15992c0
rehanzhakyy <hannn9491@gmail.com>
2024-05-25 14:54:34 +0700

    Initialize Modules String

---
+++ 01-Kotlin-Basics/11- Strings.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // Initilize String
+    val selamat = "Hello"
+
+    val selamatDatang = "Hello Guys"
+
+    val selamatDatangBelakangan: String
+    selamatDatangBelakangan = "Hello Guys telat"
+
+
+    println(selamat)
+
+    println(selamatDatang)
+
+    println(selamatDatangBelakangan)
+}
