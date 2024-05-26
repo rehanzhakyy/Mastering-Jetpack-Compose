689ee848d246b090a1823cf80090d1f9d655fd75
rehanzhakyy <hannn9491@gmail.com>
2024-05-27 02:13:47 +0700

    update Modules Conditional Statement

--- 01-Kotlin-Basics/12-Conditional-Statements.kt
+++ 01-Kotlin-Basics/12-Conditional-Statements.kt
@@ -1,6 +1,13 @@
 fun main() {
-    // if
-    if(25 > 9) {
+    val standardFinal = 75
+    val kotlinFinal = 75
+
+
+    if(kotlinFinal > standardFinal) { // if
         println("Raihan is a Kotlin Master")
+    } else if (kotlinFinal == standardFinal) { // Else If
+        println("You made it and you're right at the standard")
+    } else { // Else
+        println("Raihan is a Newbie")
     }
 }
