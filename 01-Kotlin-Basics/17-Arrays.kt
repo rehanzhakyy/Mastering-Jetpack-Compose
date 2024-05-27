1502239e4bbb0f889902347fba8d376de90f56c3
rehanzhakyy <hannn9491@gmail.com>
2024-05-27 23:59:41 +0700

    update modules arrays

--- 01-Kotlin-Basics/17-Arrays.kt
+++ 01-Kotlin-Basics/17-Arrays.kt
@@ -1,3 +1,8 @@
 fun main() {
+    val infiniteProgram = arrayOf("Web", "Mobile", "Game", "IBM", "Intern")
 
+    // Ch
+    infiniteProgram[3] = "Red Hat"
+    println(infiniteProgram[1])
+    println(infiniteProgram.size)
 }
