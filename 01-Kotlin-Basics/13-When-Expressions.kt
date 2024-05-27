1c286dbe97b922601d9b33ae94a0ee5eda4d7d4b
rehanzhakyy <hannn9491@gmail.com>
2024-05-27 22:35:12 +0700

    finished up When Expressions Module

--- 01-Kotlin-Basics/13-When-Expressions.kt
+++ 01-Kotlin-Basics/13-When-Expressions.kt
@@ -1,3 +1,15 @@
 fun main() {
+    val weekday = 4
 
+    val result = when(weekday) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "There is no Weekday"
+    }
+    println(result)
 }
