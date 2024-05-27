ce4d16a946e143eff7ae3c2cf4f300a1d5db40f8
rehanzhakyy <hannn9491@gmail.com>
2024-05-27 23:36:31 +0700

    add break & continue modules

--- 01-Kotlin-Basics/16-Break-Continue-Statements.kt
+++ 01-Kotlin-Basics/16-Break-Continue-Statements.kt
@@ -1,3 +1,15 @@
 fun main() {
+    var i = 0
+    while (i < 10) {
+        println(i)
+        i++
+
+        if (i == 6) {
+            i++
+            continue
+        } else if (i == 9) {
+            break
+        }
+    }
 
 }
