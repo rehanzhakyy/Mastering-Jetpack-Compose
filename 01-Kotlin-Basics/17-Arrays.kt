d790c72ff974d8ccf95fdbaae7a5a98ffb03f5b9
rehanzhakyy <hannn9491@gmail.com>
2024-05-28 00:15:44 +0700

    finished up module Array

--- 01-Kotlin-Basics/17-Arrays.kt
+++ 01-Kotlin-Basics/17-Arrays.kt
@@ -1,8 +1,17 @@
 fun main() {
     val infiniteProgram = arrayOf("Web", "Mobile", "Game", "IBM", "Intern")
 
-    // Ch
+    // Change the value of Arrays
     infiniteProgram[3] = "Red Hat"
+
+    // Printing the index of Arrays
     println(infiniteProgram[1])
+
+    // Printing the size of Arrays
     println(infiniteProgram.size)
+
+    // For loop in Array
+    for (x in infiniteProgram) {
+        println(x)
+    }
 }
