be01b48318c016fb1a6af337e76b781c9a24f69f
rehanzhakyy <hannn9491@gmail.com>
2024-05-30 20:14:15 +0700

    add ranges - Number

--- 01-Kotlin-Basics/19-Ranges.kt
+++ 01-Kotlin-Basics/19-Ranges.kt
@@ -1,3 +1,11 @@
 fun main() {
+    // Initialize Ranges - Chars
+    for(chars in 'a' .. 'z') {
+        println(chars)
+    }
 
+    // Ranges - Number
+    for(x in 1..20) {
+        println(x)
+    }
 }
