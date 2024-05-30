33c8daf315ec676da4a00f69976ff3194ca49e6d
rehanzhakyy <hannn9491@gmail.com>
2024-05-30 20:28:26 +0700

    add range - check availability

--- 01-Kotlin-Basics/19-Ranges.kt
+++ 01-Kotlin-Basics/19-Ranges.kt
@@ -8,4 +8,12 @@ fun main() {
     for(x in 1..20) {
         println(x)
     }
+
+    // Ranges - Check Availability
+    val validation = arrayOf(1,13,35,42,4242,5245,233,235,35,25)
+    if (13 in validation) {
+        println("It's Exist")
+    } else {
+        println("It's doesn't Exist")
+    }
 }
