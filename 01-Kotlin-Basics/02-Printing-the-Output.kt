be2cafc43bca38baed7904bde5b93bb669350301
rehanzhakyy <hannn9491@gmail.com>
2024-06-07 18:38:30 +0700

    add higher order function

--- 01-Kotlin-Basics/02-Printing-the-Output.kt
+++ 01-Kotlin-Basics/02-Printing-the-Output.kt
@@ -1,7 +1,24 @@
 fun main() {
-
+    // HOF
+    printResult(50, sum)
 }
 
-fun printResult() {
-
+fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
 }
+
+var sum: (Int) -> Int = {value -> value + value}
+
+
+/*fun main() {
+    printResult(50) { value ->
+        value + value
+    }
+}
+fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
+}
+ */
+
