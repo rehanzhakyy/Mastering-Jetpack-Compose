841d9d440c8ef64ae087d616a106c8ead8428486
rehanzhakyy <hannn9491@gmail.com>
2024-06-07 18:33:39 +0700

    add lambda

--- 01-Kotlin-Basics/01-Kotlin-Syntax.kt
+++ 01-Kotlin-Basics/01-Kotlin-Syntax.kt
@@ -1,13 +1,7 @@
-data class minjemBuku(val name: String, val publisher: String, val review: Double)
-
 fun main() {
-    // Inisialisasi
-    val minjemBuku = minjemBuku("Raihan", "Amabel Media", 4.8)
-    // Copy
-    val minjemBukuCopy = minjemBuku.copy()
-    // Modify
-    val minjemBukuModify = minjemBuku.copy("Fiastara", "Gatot Jaya Makmur", 5.0)
+    // lambda
+    val sum = { x: Int, y: Int -> x + y }
 
-    println(minjemBukuCopy)
-    println(minjemBukuModify)
+    val result = sum(10, 20)
+    println(result)
 }
--- 01-Kotlin-Basics/02-Printing-the-Output.kt
+++ 01-Kotlin-Basics/02-Printing-the-Output.kt
@@ -1,6 +1,7 @@
 fun main() {
-    val banyakAngka = 1..5
-    banyakAngka.forEach { value ->
-        println("value is $value!")
-    }
+
+}
+
+fun printResult() {
+
 }
