e9b8b8062187ad00899d091b542815afeda247c3
rehanzhakyy <hannn9491@gmail.com>
2024-06-09 21:14:00 +0700

    fixed printing the output

--- 01-Kotlin-Basics/02-Printing-the-Output.kt
+++ 01-Kotlin-Basics/02-Printing-the-Output.kt
@@ -1,24 +1,5 @@
-/*fun main() {
-    // HOF
-    printResult(50, sum)
-}
-
-fun printResult(value: Int, sum: (Int) -> Int) {
-    val result = sum(value)
-    println(result)
-}
-
-var sum: (Int) -> Int = {value -> value + value}
-
- */
-
 fun main() {
-    printResult(50) { value ->
-        value + value + value
-    }
-}
-fun printResult(value: Int, sum: (Int) -> Int) {
-    val result = sum(value)
-    println(result)
-}
+    val company = "Infinite Learning"
 
+    println(company)
+}
