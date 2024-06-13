e884a16544103d358e721a03c954b6da701183a2
rehanzhakyy <hannn9491@gmail.com>
2024-06-13 20:20:03 +0700

    Update Kotlin Syntax

--- 01-Kotlin-Basics/01-Kotlin-Syntax.kt
+++ 01-Kotlin-Basics/01-Kotlin-Syntax.kt
@@ -1,22 +1,3 @@
-class gatot(
-    private var nama: String,
-    private var berat: Double,
-    var umur: Int,
-    var apkhNyata: Boolean = true
-) {
-    fun getName(): String {
-        return nama
-    }
-
-    fun setNama(newNama: String) {
-        nama = newNama
-    }
-}
-
 fun main() {
-    val menteeBootcamp = gatot("gagah berotot", 1.5, 39)
-    println(menteeBootcamp.getName())
-    menteeBootcamp.setNama("raihan")
-    println(menteeBootcamp.getName())
-
+    println("Hello World!")
 }
